e1298619d1366fdf80a0b1b607d540940a03efa8
John Herreño <jedihe@gmail.com>
2020-08-23 17:53:22 -0500

    fmt-build-drush-phar.sh: remove compactors for box2, as the resulting phar is broken (no commands shown when calling "drush list")

--- drupal8/drush-phar/fmt-build-drush-phar.sh
+++ drupal8/drush-phar/fmt-build-drush-phar.sh
@@ -81,10 +81,6 @@ BOX_JSON=$(cat <<-SNIPPET
             "in": "vendor"
         }
     ],
-    "compactors": [
-        "Herrera\\\\Box\\\\Compactor\\\\Json",
-        "Herrera\\\\Box\\\\Compactor\\\\Php"
-    ],
     "main": "vendor/drush/drush/drush.php",
     "output": "drush-$DRUSH_VERSION.phar",
     "stub": true
